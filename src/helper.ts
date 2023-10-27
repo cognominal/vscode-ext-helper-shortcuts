3f1efdfd1df7ef547fc2d99f2622527aa3daf302
Stéphane Payrard <cognominal@gmail.com>
2023-10-27 18:11:10 +0200

    adding more entries to driver

--- src/helper.ts
+++ src/helper.ts
@@ -28,8 +28,8 @@ type Driver = {
 let driver: Driver = {
 	"General": {
 		"Show Command Palette": { key: "F1", id: "workbench.action.showCommands" },
-		"File: open file ": { key: "⌘P", wkey: "Ctrl+P",  id:"workbench.action.files.openFile" },
-		
+		"File: open file ": { key: "⌘P", wkey: "Ctrl+P", id: "workbench.action.files.openFile" },
+
 	},
 	"Basic Editing": {
 		"Cut selection. Cut line on empty selection": { key: "⌘X", id: "editor.action.clipboardCutAction" },
@@ -39,33 +39,21 @@ let driver: Driver = {
 		"Insert cursor above": { key: "⌥⌘↑", id: "editor.action.insertCursorAbove" },
 		"Insert cursor below": { key: "⌥⌘↓", id: "editor.action.insertCursorBelow" },
 		"": { key: "", id: "" },
-		// ⇧⌥I 
 		"Insert cursor at end of each line selected": { key: "⇧⌥I", id: "editor.action.insertCursorAtEndOfEachLineSelected" },
-		// ⌘L 
 		"Select current line": { key: "⌘L", id: "editor.action.selectLines" },
-		// ⇧⌘L 
 		"Select all occurrences of current selection": { key: "⇧⌘L", id: "editor.action.selectHighlights" },
-		// ⌘F2 
 		"Select all occurrences of current word": { key: "⌘F2", id: "editor.action.changeAll" },
-		// ⌃⇧⌘→ / ← 
 		"Expand selection": { key: "⌃⇧⌘→", id: "editor.action.smartSelect.expand" },
 		"Shrink selection": { key: "⌃⇧⌘→", id: "editor.action.smartSelect.shrink" },
-		// ⇧⌥ + "drag mouse Column (box) selection":
-		// ⇧⌥⌘↑ 
 		"Column (box) selection up": { key: "⇧⌥⌘↑", id: "editor.action.insertCursorAbove" },
-		// ⇧⌥⌘↓ 
 		"Column (box) selection down": { key: "⇧⌥⌘↓", id: "editor.action.insertCursorBelow" },
-		// ⇧⌥⌘←  
 		"Column (box) selection left": { key: "⇧⌥⌘←", id: "editor.action.columnSelectLeft" },
-		// ⇧⌥⌘→ 
 		"Column (box) selection right": { key: "⇧⌥⌘→", id: "editor.action.columnSelectRight" },
-		// ⇧⌥⌘PgUp 
-		"Column (box) selection page up": { key: "⇧⌥⌘PgUp", id: "editor.action.columnSelectPageUp"},
-		// ⇧⌥⌘PgDn 
-		"Column (box) selection page down": { key: "⇧⌥⌘PgDn", id: "editor.action.columnSelectPageDown" }, 
+		"Column (box) selection page up": { key: "⇧⌥⌘PgUp", id: "editor.action.columnSelectPageUp" },
+		"Column (box) selection page down": { key: "⇧⌥⌘PgDn", id: "editor.action.columnSelectPageDown" },
 	},
 	"Search and replace": {
-		"Find": {  key: "⌘F", id: "actions.find" },
+		"Find": { key: "⌘F", id: "actions.find" },
 	},
 	"Rich languages editing": {
 		"Show all symbols": { id: "workbench.action.gotoSymbol" },
@@ -77,16 +65,42 @@ let driver: Driver = {
 	"File management": {
 	},
 	"Display": {
+		"Toggle full screen": { key: "⌃⌘F", id: "workbench.action.toggleFullScreen" },
+		"Toggle editor layout (horizontal/vertical)": { key: "⌥⌘0", id: "workbench.action.toggleEditorGroupLayout" },
+		"Zoom in": { key: "⌘=", id: "workbench.action.zoomIn" },
+		"Zoom out": { key: "⇧⌘-", id: "workbench.action.zoomOut" },
+		"Toggle Sidebar visibility": { key: "⌘B", id: "workbench.action.toggleSidebarVisibility" },
+		"Show Explorer / Toggle focus": { key: "⇧⌘E", id: "workbench.view.explorer" },
+		"Show Search": { key: "⇧⌘F", id: "workbench.view.search" },
+		"Show Source Control": { key: "⌃⇧G", id: "workbench.view.scm" },
+		"Show Debug": { key: "⇧⌘D", id: "workbench.view.debug" },
+		"Show Extensions": { key: "⇧⌘X", id: "workbench.view.extensions" },
+		"Replace in files": { key: "⇧⌘H", id: "workbench.action.replaceInFiles" },
+		"Toggle Search details": { key: "⇧⌘J", id: "search.action.toggleQueryDetails" },
+		"Show Output panel": { key: "⇧⌘U", id: "workbench.action.output.toggleOutput" },
+		"Open Markdown preview": { key: "⇧⌘V", id: "markdown.showPreview" },
+		"Open Markdown preview to the side": { key: "⌘K V", id: "markdown.showPreviewToSide" },
+		"Zen Mode (Esc Esc to exit)": { key: "⌘K Z", id: "workbench.action.toggleZenMode" },
 	},
 	"Debug": {
+		"Toggle breakpoint": { key: "F9", id: "editor.debug.action.toggleBreakpoint" },
+		"Start/Continue": { key: "F5", id: "workbench.action.debug.start" },
+		"Step into": { key: "F11", id: "workbench.action.debug.stepInto" },
+		"Step out": { key: "⇧F11", id: "workbench.action.debug.stepOut" },
+		"Step over": { key: "F10", id: "workbench.action.debug.stepOver" },
+		"Stop": { key: "⇧F5", id: "workbench.action.debug.stop" },
+		"Show hover": { key: "⌘K ⌘I", id: "editor.debug.action.showDebugHover" },
 	},
 	"Integrated terminal": {
-		// ⌃` Show integrated terminal
-		// ⌃⇧` Create new terminal
-		// ⌘C Copy selection
-		// ⌘↑ / ↓ Scroll up/down
-		// PgUp / PgDn Scroll page up/down
-		// ⌘Home / End Scroll to top/bottom		
+		"Show integrated terminal": { key: "⌃`", id: "workbench.action.terminal.toggleTerminal" },
+		"Create new terminal": { key: "⌃⇧`", id: "workbench.action.terminal.new" },
+		"Copy selection": { key: "⌘C", id: "workbench.action.terminal.copySelection" },
+		"Scroll up": { key: "⌘↑", id: "workbench.action.terminal.scrollUp" },
+		"Scroll down": { key: "⌘↓", id: "workbench.action.terminal.scrollDown" },
+		"Scroll page up": { key: "PgUp", id: "workbench.action.terminal.scrollUpPage" },
+		"Scroll page down": { key: "PgDn", id: "workbench.action.terminal.scrollDownPage" },
+		"Scroll to top": { key: "⌘Home", id: "workbench.action.terminal.scrollToTop" },
+		"Scroll to bottom": { key: "⌘End", id: "workbench.action.terminal.scrollToBottom" },
 	}
 }
 
@@ -94,10 +108,10 @@ let driver: Driver = {
 export function helperShortcuts(context: ExtensionContext) {
 	const quickPick = window.createQuickPick();
 	quickPick.items = Object.keys(driver).map(label => ({ label }));
-	
+
 	quickPick.onDidChangeSelection(selection => {
 		quickPick.hide();
-		
+
 		if (selection[0]) {
 			let strpicked: string = selection[0].label
 			subQuickPick(strpicked)
@@ -108,7 +122,7 @@ export function helperShortcuts(context: ExtensionContext) {
 	quickPick.show();
 };
 
-function subQuickPick(strpicked : string) {
+function subQuickPick(strpicked: string) {
 	let picked: Category = driver[strpicked];
 	const subquickPick = window.createQuickPick();
 	subquickPick.items = Object.keys(picked).map(label => {
@@ -125,7 +139,7 @@ function subQuickPick(strpicked : string) {
 	}
 	);
 	subquickPick.onDidChangeSelection(selection => {
-		
+
 		if (selection[0]) {
 			let strpicked: string = selection[0].label
 			if (picked[strpicked].id) {
@@ -136,7 +150,7 @@ function subQuickPick(strpicked : string) {
 			}
 		}
 	});
-	
+
 	subquickPick.onDidHide(() => subquickPick.dispose());
 	subquickPick.show();
 
